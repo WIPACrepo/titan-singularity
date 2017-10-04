8b60b41b771b100f4fab9e9722caa5f310f39641
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2017-10-04 10:25:51 -0500

    minor updates

--- make_dir_container.sh
+++ make_dir_container.sh
@@ -4,9 +4,9 @@ src=$1
 img=${src%.*}
 
 ./concat.sh $src > scratch/$img.def && \
-	(sudo rm -rf scratch/$img; \
-	mkdir scratch/$img && \
-	sudo singularity bootstrap scratch/$img scratch/$img.def)
+	(sudo rm -rf scratch/$img.d; \
+	mkdir scratch/$img.d && \
+	sudo singularity bootstrap scratch/$img.d scratch/$img.def)
 
 #sudo chown -R vbrik:vbrik scratch/$img
 #sudo chmod -R u+rw scratch/$img
